3bf54513949dc018613b8ae9748c2df7e17a173f
ChangHong <1287380598@qq.com>
2020-12-11 15:15:30 +0800

    用户下拉刷新数据

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -119,7 +119,19 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-      
+      console.log('%c 上拉到底',' text-shadow: 0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);font-size:5em;')
+      /* 
+        1.重置列表数据
+        2.重置页码
+        3.重新发送请求
+        4.加载完成手动关闭刷新
+      */
+     this.setData({
+       goodslist:[]
+     })
+     this.QueryParams.pagenum = 1;
+     this.getGoodList()
+     wx.stopPullDownRefresh()
   },
 
   /**
