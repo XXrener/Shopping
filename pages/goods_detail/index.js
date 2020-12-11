98dafb1a700b1ae36e552d618f4612baadc8e573
ChangHong <1287380598@qq.com>
2020-12-11 19:16:23 +0800

    商品详情页 动态渲染

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,20 +1,33 @@
 // pages/goods_detail/index.js
+import { request } from "../../requset/index";
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    detailList:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    /* 页面加载时获取详情 */
+    const {goods_id } = options;
+    this.getDdetail(goods_id)
+    console.log(goods_id)
 
   },
-
+  /*详情请求 get */
+  async getDdetail(id){
+    console.log(id,"请求内的ID")
+    let detailList = await request({url:'api/public/v1/goods/detail',data:{goods_id:id}})
+    this.setData({
+      detailList
+    })
+    console.log(this.data.detailList,"赋值参数")
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
