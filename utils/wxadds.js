b806cc1e11f84521cc25c3f0edcce4cf5e3e5acf
ChangHong <1287380598@qq.com>
2020-12-14 13:54:40 +0800

    封装微信原生模态框提示

--- utils/wxadds.js
+++ utils/wxadds.js
@@ -12,5 +12,23 @@ module.exports={
             });
               
         })
+    },
+     /* 提示模态框封装 */
+     showmodal:(content)=>{
+        return new Promise( (resolve,reject)=>{
+            wx.showModal({
+                title: '提示',
+                content,
+                showCancel: true,
+                success: (result) => {
+                    if (result.confirm) {
+                        resolve(result)
+                    }
+                },
+                fail: (err) => { reject(err)},
+                complete: () => {}
+            });
+              
+        })
     }
 }
